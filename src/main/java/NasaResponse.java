d7041e93c93ff92847b643d0f1a13614d0ea1e85
StreLkoO <strelkooo@gmail.com>
2022-06-22 22:38:50 +0300

    Fixed NasaResponse

--- src/main/java/NasaResponse.java
+++ src/main/java/NasaResponse.java
@@ -5,8 +5,8 @@ public class NasaResponse {
     private final String date;
     private final String explanation;
     private final String hdurl;
-    private final String media_type;
-    private final String service_version;
+    private final String mediaType;
+    private final String serviceVersion;
     private final String title;
     private final String url;
 
@@ -15,8 +15,8 @@ public class NasaResponse {
             @JsonProperty("date") String date,
             @JsonProperty("explanation") String explanation,
             @JsonProperty("hdurl") String hdurl,
-            @JsonProperty("media_type") String media_type,
-            @JsonProperty("service_version") String service_version,
+            @JsonProperty("media_type") String mediaType,
+            @JsonProperty("service_version") String serviceVersion,
             @JsonProperty("title") String title,
             @JsonProperty("url") String url
     ) {
@@ -24,8 +24,8 @@ public class NasaResponse {
         this.date = date;
         this.explanation = explanation;
         this.hdurl = hdurl;
-        this.media_type = media_type;
-        this.service_version = service_version;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
         this.title = title;
         this.url = url;
     }
@@ -46,12 +46,12 @@ public class NasaResponse {
         return hdurl;
     }
 
-    public String getMedia_type() {
-        return media_type;
+    public String getMediaType() {
+        return mediaType;
     }
 
-    public String getService_version() {
-        return service_version;
+    public String getServiceVersion() {
+        return serviceVersion;
     }
 
     public String getTitle() {
